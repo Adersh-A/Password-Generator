33aacc4826799cd59ca1b966fffd6b03a647e095
Adersh A <adershajith1999@gmail.com>
2023-07-23 17:32:25 +0530

    added password generator code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { isEmpty } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,64 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'password_generator';
+  
+  password = ''
+  includeLtrs = false;
+  includeNmbrs = false;
+  includeSym = false;
+  length = 0;
+
+  onChangeInput(value: string){
+    const parsedValue = parseInt(value);
+    if(!isNaN(parsedValue)){
+      this.length = parsedValue;
+    }
+  }
+
+  includeLetters(){
+    this.includeLtrs = !this.includeLtrs;
+  }
+  includeNumbers(){
+    this.includeNmbrs = !this.includeNmbrs;
+  }
+  includeSymbols(){
+    this.includeSym = !this.includeSym;
+  }
+
+  onButtonClick(){
+    console.log(this.includeLtrs);
+    console.log(this.includeNmbrs);
+    console.log(this.includeSym);
+    console.log(this.length);
+
+    
+
+    const validLetters = 'abcdefghijklmnopqrstuvwxyz'
+    const validNumbers = '1234567890'
+    const validSymbols = '!@#$%^&*'
+
+    let validString = '';
+
+    if(this.includeLtrs){
+      validString+=validLetters;
+    }
+    if(this.includeNmbrs){
+      validString+=validNumbers;
+    }
+    if(this.includeSym){
+      validString+=validSymbols;
+    }
+
+    let generatedPass = ''
+
+    for(let i=0; i<this.length; i++){
+      const index = Math.floor(Math.random() * validString.length);
+      generatedPass+= validString[index];
+    }
+    
+    this.password = generatedPass;
+    
+  }
+
+
 }
